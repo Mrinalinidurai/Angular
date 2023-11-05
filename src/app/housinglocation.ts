a47acab361f8b004294ec64de9164597c9933de8
Mrinalinidurai <91600903+Mrinalinidurai@users.noreply.github.com>
2023-11-05 23:10:25 +0530

    Add files via upload

---
+++ src/app/housinglocation.ts
@@ -0,0 +1,18 @@
+export interface HousingLocation {
+  id: number;
+  name: string;
+  city: string;
+  state: string;
+  photo: string;
+  avgcost: number;
+  timings: string;
+  wifi: boolean;
+ 
+}
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
